fc895c7da32434768d6bc442ab581c9c258399cf
birdiecode <birdiecode@protonmail.com>
2021-12-24 06:55:52 +0300

    добавил sleep

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #define SPRITE "<`)     //<`)// (//(`>/ (`> (/  (\\\\  1  \\  \"birdiecode\n"
 long re;
 
-
 struct timespec {
-	long tv_sec;        /* seconds */
-	long tv_nsec;       /* nanoseconds */
-};
+long int tv_sec; // seconds
+long tv_nsec; // and nanoseconds
+} T = {1, 0};
+
 
 void _start() __asm__("_start");
 
@@ -13,30 +13,18 @@ void strcc(char s, char n)
 {
 	for (; s < n; ++s)
 	{
-		__asm__ volatile ("int $0x80": "=a"(re): "0"(4), "b"(1), "c"((long)&SPRITE[s]), "d"((long)(1))); //write(1, &SPRITE[s], 1);
+		__asm__ volatile ("int $0x80": "=a"(re): "0"(4), "b"(1), "c"((long)&SPRITE[s]), "d"((long)(1)));
 	}
-
 	__asm__ volatile ("int $0x80": "=a"(re): "0"(4), "b"(1), "c"((long)&SPRITE[54]), "d"((long)(1)));
+	
 }
 
 void _start()
 {
-	struct timespec T;
-	T.tv_sec = 1;
-	T.tv_nsec = 1000000;
-
 	strcc(0, 4);
 	strcc(15,19);
 	strcc(35,39);
 	strcc(44,54);
-
-	// sleep 
-	//asm("movl eax, 162");
-	//asm("movl ebx, T");
-	//asm("movl ecx, 0");
-	//asm("int $0x80");
-	// or
-	//__asm__ volatile ("int $0x80": "=a"(re): "0"(162), "b"((long)&T), "c"(0));
-
+	__asm__ volatile ("int $0x80": "=a"(re): "0"(162), "b"((long)&T), "c"(0)); //sleep
 	__asm__ volatile ("int $0x80": "=a"(re): "0"(1),"b" (0));
 }
