a4f53c8d589f8d5d25046fb10204dd312edf39aa
birdiecode <birdiecode@protonmail.com>
2021-12-24 07:39:01 +0300

    сделана анимация

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#define SPRITE "<`)     //<`)// (//(`>/ (`> (/  (\\\\  1  \\  \"birdiecode\n"
+#define SPRITE "<`)     //<`)// (//(`>/ (`> (/  (\\\\  1  \\  \"birdiecode\n\033[5A"
 long re;
 
 struct timespec {
@@ -31,12 +31,14 @@ char a[2][3][2] = {
 
 void _start()
 {
-	int ind = 1;
-
-	strcc(a[ind][0][0], a[ind][0][1]);
-	strcc(a[ind][1][0], a[ind][1][1]);
-	strcc(a[ind][2][0], a[ind][2][1]);
-	strcc(44,54);
-	__asm__ volatile ("int $0x80": "=a"(re): "0"(162), "b"((long)&T), "c"(0)); //sleep
+	for (int i = 0; i < 2; ++i)
+	{
+		strcc(a[i][0][0], a[i][0][1]);
+		strcc(a[i][1][0], a[i][1][1]);
+		strcc(a[i][2][0], a[i][2][1]);
+		strcc(44,54);
+		__asm__ volatile ("int $0x80": "=a"(re): "0"(162), "b"((long)&T), "c"(0)); //sleep
+		strcc(55, 59);
+	}
 	__asm__ volatile ("int $0x80": "=a"(re): "0"(1),"b" (0));
 }
