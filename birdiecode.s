9202772d27c1f05a7656300506ec495565ac3a89
birdiecode <birdiecode@gmail.com>
2024-09-30 14:02:55 +0300

    написал смещение для отображения фрагментов фрейма

--- birdiecode.s
+++ birdiecode.s
@@ -1,11 +1,19 @@
 section .data
   msg db '<`)   //<`)// (\\   "  1  (>/  (/ (//   \'
+  frames db 0, 34, 21, 3, 26, 21, 3, 25, 21, 8, 30, 21, 0, 13, 38, 8, 30, 17, 0, 13, 17
+  frame db 1
 
 section .text
   global _start
 
 _start:
-  mov rsi, msg
+  mov rdi, frames
+  movzx rsi, byte [frame] ; Загружаем значение из адреса frame в регистр rsi
+  add rdi, rsi            ; Смещаем указатель в массиве на индекс, указанный в rsi. На этом этапе rsi больше не нужен
+  movzx rax, byte [rdi]   ; Получаем значение из массива frames, то есть начальный адрес frames плюс смещение frame
+  mov rsi, msg            ; Записываем в rsi адрес msg
+  add rsi, rax            ; Сдвигаемся на значение из массива
+
   mov rax, 1
   mov rdi, 1
   mov rdx, 5
